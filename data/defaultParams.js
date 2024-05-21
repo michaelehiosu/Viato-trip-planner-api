03c61410c7e82f858ed285ab604cfd39b0a46044
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2024-05-21 16:31:36 +0200

    Cleaned up the file structure for firebase integration

--- data/defaultParams.js
+++
@@ -1,6 +0,0 @@
-const defaultHeaders = {
-  "X-RapidAPI-Key": "5aca35d180msh3ca3ef11033f451p12b26djsn3752f71b67e1",
-  "X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com",
-};
-
-module.exports = { defaultHeaders };
--- services/stays.js
+++
@@ -1,1 +0,0 @@
-// STAYS FUNCTIONS
